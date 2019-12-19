36452b4f721149f4f4615066d9de86deab051730
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-12-18 18:32:09 -0600

    removed print statement

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -130,7 +130,6 @@ class CPU:
                 reg_val = self.reg[reg_num]
                 self.ram[self.reg[SP]] = reg_val
                 self.pc += 2
-                print(self.reg)
             elif instruction_register == HLT:
                 flag = False
                 break
